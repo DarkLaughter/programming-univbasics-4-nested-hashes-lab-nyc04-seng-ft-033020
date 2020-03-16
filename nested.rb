4ccf19cd51ce60d0cfb93cf12b552e7853960e6d
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-16 15:51:28 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,6 +57,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+
 		programmer_hash[:dennis_ritchie][:languages]
 end
 
@@ -109,6 +110,7 @@ def changing_alan
       }
     }
 
+		programmer_hash[:alan_kay][:known_for] = "GUI"
 end
 
 def adding_to_dennis
