8de193d9afbc72fbd45a6dbd3ac46ff23b636f7f
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-16 16:11:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -112,7 +112,7 @@ def changing_alan
 		new_known = "GUI"
 
 		programmer_hash[:alan_kay][:known_for] = new_known
-		programmer_hash[:alan_kay][:known_for]
+		programmer_hash
 end
 
 def adding_to_dennis
